19230ef9e7700d3cf476bbc244eb027948a84158
Talles Gazel <tjgazel@gmail.com>
2018-05-20 01:07:01 -0300

    add style in tag p

--- views/template.blade.php
+++ views/template.blade.php
@@ -6,10 +6,10 @@
         <span aria-hidden="true">&times;</span>
       </button>
       @if($bs3Alert['title'])
-        <h5>{!! $bs3Alert['title'] !!}</h5>
+        <p><strong>{!! $bs3Alert['title'] !!}</strong></p>
       @endif
       @foreach($bs3Alert['messages'] as $bs3AlertMessage)
-        <p>{!! $bs3AlertMessage !!}</p>
+        <p style="line-height: 1.1em">{!! $bs3AlertMessage !!}</p>
       @endforeach
     </div>
   @endforeach
